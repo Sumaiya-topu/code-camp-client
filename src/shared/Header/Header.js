29a97af02973d06c93e83d6d07b82ebe7182e3a1
Sumaiya-topu <topusumaiya@gmail.com>
2022-10-27 11:41:02 +0600

    login with google done

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useContext } from 'react';
+import { Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import { FaUser } from 'react-icons/fa';
 
 
 
@@ -36,11 +38,27 @@ const Header = () => {
                             <Link className='text-decoration-none text-white px-4 py-2 ' to="/courses">Courses</Link>
                             <Link className='text-decoration-none text-white px-4 py-2 ' to="/faq">FAQ</Link>
                             <Link className='text-decoration-none text-white px-4 py-2 ' to="/blog">BLOG</Link>
-                            <Link className='text-decoration-none  text-white px-4 py-2 ' to="/blog">Log In</Link>
-                            <Link className='text-decoration-none bg-light px-4 py-2 rounded text-warning fw-bold ' to="/blog">Sign Up</Link>
-                            <Link className='text-decoration-none text-white ' eventKey={2} to="#memes">
-                                {user.displayName}
-                            </Link>
+
+                            {
+                                user.email ?
+
+                                    <Link className='text-decoration-none text-white px-4 py-2 ' eventKey={2} to="#memes">
+
+                                        {
+                                            user.displayName
+                                        }
+                                        {
+                                            user.photoURL ? <Image className='ms-2' style={{ height: '30px' }} roundedCircle src={user.photoURL}></Image> : <FaUser></FaUser>
+
+                                        }
+                                    </Link>
+                                    : <div>
+                                        <Link className='text-decoration-none  text-white px-4 py-2 ' to="/blog">Log In</Link>
+                                        <Link className='text-decoration-none bg-light px-4 py-2 rounded text-warning fw-bold ' to="/blog">Sign Up</Link>
+                                    </div>
+                            }
+
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
